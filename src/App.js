b9dc1061a892461e4a462272cdf1b5c9dd148ebe
kevan <kevanpngyeowwei@gmail.com>
2018-10-17 18:52:37 +0800

    take from config.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+import { tokenUrl, instanceLocator,userId, roomId } from './config'
+
 class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            messages: []
+        }
+    }
+
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
+            userId: userId,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: tokenUrl
+            })
+        })
+
+        chatManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                roomId: roomId,
+                hooks: {
+                    onNewMessage: message => {
+                        this.setState({
+                            messages: [...this.state.messages, message]
+                        })
+                    }
+                }
+            })
+        })
+    }
+
     render() {
         return (
             <div className="app">
                 <RoomList />
-                <MessageList />
+                <MessageList messages={this.state.messages} />
                 <SendMessageForm />
                 <NewRoomForm />
             </div>
